153453f46d26fe9be31bb19da984bfa1db7f98d4
Adeyemi7 <integraadeyemi11@gmail.com>
2022-10-17 23:31:51 +0100

    test

---
+++ modal.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector(".firstt");
+
+let pro = document.querySelector('.first')
+
+let main = document.querySelector(".modal");
+
+let back = document.querySelector(".close");
+
+ btn.addEventListener('click', () => {
+
+modal.body.style.opacity = '1'
+
+console.log ('hello modal')
+})
+
+back.addEventListener('click', () => {
+    
+    })
